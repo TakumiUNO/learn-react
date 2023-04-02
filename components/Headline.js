f7f7cf42f0a4953291eae78996874bab33705f41
UNO <58221131+TakumiUNO@users.noreply.github.com>
2023-04-02 11:38:40 +0900

    ✨ ヘッダー追加

--- components/Headline.js
+++ components/Headline.js
@@ -11,11 +11,8 @@ export function Headline(props) {
     <div className={classes.Headline}>
       <h1 className={classes.title}>{props.title} Page</h1>
 
-      <a href={link}>{props.link}</a>
-
       <p className={classes.description}>
         Get started by editing
-        {/* <code className={classes.code}>pages/{props.link}.js</code> */}
         {props.children}
       </p>
     </div>
