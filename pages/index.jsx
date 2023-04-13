8d65d14a6d5d92e61ed2ca029d8d3c088ef77b7f
UNO <58221131+TakumiUNO@users.noreply.github.com>
2023-04-13 20:26:30 +0900

    ✨ useEffectを導入

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,26 @@ import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    console.log("マウント");
+    return () => {
+      document.body.style.backgroundColor = "";
+      console.log("アンマウント");
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +30,9 @@ export default function Home() {
       </Head>
 
       <Header></Header>
+      <a href="/about" onClick={handleClick}>
+        button
+      </a>
 
       <Main title="index" link="about" />
 
